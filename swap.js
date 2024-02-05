70f01aa6ffcb6adc5e613d08a0f63df1fa26a157
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-05 18:03:37 +0600

    Finished Module

--- swap.js
+++ swap.js
@@ -16,8 +16,16 @@ b = temp;
 let x = 5;
 let y = 7;
 
-console.log(x, y);
+// console.log(x, y);
 
 [x, y] = [y, x];
 
-console.log(x, y);
+// console.log(x, y);
+
+// for (var i = 0; i < 5; i++) {
+//     setTimeout(function () {
+//      console.log(i);
+//   }, 100);
+// }
+
+console.log(Math.pow(2, 3));
